b878a5985fa358cd88b433c6980f2d236218108e
KISS <me@luisam.com>
2019-07-19 21:08:36 -0400

    Added subscribe form, additional fonts, updated styles

--- js/main.js
+++ js/main.js
@@ -4,7 +4,31 @@ $(document).ready(function () {
   });
 
   $('.section-aside .js-link').on('click', function (e) {
-    // https://github.com/kiss/web-accessibility-workshop
-    window.open('https://github.com/kiss');
+    window.open('https://github.com/kiss/web-accessibility-workshop');
+  });
+
+  $('.form-subscribe .button').on('click', function (e) {
+    const $inputs = $('.form-subscribe input');
+    let valid = true;
+
+    for (let i = 0; i < $inputs.length; i++) {
+      console.log($inputs[i].classList)
+      if (!$inputs[i].validity.valid) {
+        valid = false;
+        $inputs[i].classList = ('error');
+      } else {
+        $inputs[i].classList = ('');
+      }
+    }
+
+    if (!valid) {
+      $('.form-subscribe .error').show();
+    } else {
+      $('.form-subscribe input').val('');
+      $('.form-subscribe input[type=checkbox]').prop('checked',false);
+
+      $('.form-subscribe .error').hide();
+      $('.form-subscribe .success').show();
+    }
   });
 });
